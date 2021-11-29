38e162d8fd8b0c128a8ffbad56818b6011d33907
GiHunit <socassi777@gmail.com>
2021-11-29 21:37:10 +0100

    first commit

---
+++ P5-Dev-Web-Kanap/front/js/script.js
@@ -0,0 +1,26 @@
+const apiUrl = "http://localhost:3000/api/products"
+const items = document.getElementById("items")
+
+fetch (apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        for(product of data){
+            items.innerHTML += addProductTfront(product)
+
+        }
+    })
+    .catch((error) => console.log("il y a des erreurs sur le chargemnent de l'Api" + error)) 
+    
+function addProductTfront(product) {
+    return  ` <a href="./product.html?id=${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="${product.altTxt}">
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+          </a>`
+          
+    
+}
+
