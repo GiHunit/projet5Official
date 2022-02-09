318b9f852b17bcd1fe20fa15c21c70c462f864a4
GiHunit <socassi777@gmail.com>
2022-02-09 20:47:17 +0100

    formulaire

--- P5-Dev-Web-Kanap/front/js/cart.js
+++ P5-Dev-Web-Kanap/front/js/cart.js
@@ -1,80 +1,85 @@
 const items = document.getElementById("cart__items")
-let totalQuantity = 0
-const elemTotalQuantity = document.getElementById("totalQuantity")
-
-let  totalPrice = 0
-const elemTotalPrice = document.getElementById('totalPrice')
-
-
-
+const formCart = document.querySelector(".cart__order__form")
+const apiUrl = "http://localhost:3000/api/products/order"
 // creer une variable qui est egal au storage si pas null sinon tableau vide
 
 let panier = JSON.parse(localStorage.getItem("panier")) || [];
-
 for (let item of panier) {
-  console.log(item.quantity)
-  items.innerHTML += addPanierThird(item) 
-  totalQuantity += item.quantity
-  totalPrice += item.price
+  items.innerHTML += addPanierThird(item)
 }
+updateCartValues();
 
-elemTotalQuantity.innerHTML = totalQuantity
-
-elemTotalPrice.innerHTML = totalPrice
 
 const deleteBtns = document.querySelectorAll(".deleteItem")
 deleteBtns.forEach(btn => {
-  btn.addEventListener("click",  () => {
-    console.log(btn.closest('.cart__item').getAttribute('data-id'))
-    console.log(btn.closest('.cart__item').getAttribute('data-color'));
-    // recup le panier + filtrer avec les elements et les supprimer
+  btn.addEventListener("click", () => {
+    const cardItemDiv = btn.closest(".cart__item")
+    panier = panier.filter(product => {
+      return product.id !== cardItemDiv.getAttribute('data-id')
+        && product.color !== cardItemDiv.getAttribute('data-color')
     })
+    localStorage.setItem("panier", JSON.stringify(panier))
+    cardItemDiv.remove()
+    updateCartValues()
+  })
+})
+const itemQuantity = document.querySelectorAll('.itemQuantity')
+
+itemQuantity.forEach(item => {
+  item.addEventListener("click", () => {
+    const cardItemDiv = item.closest(".cart__item")
+    panier = panier.map(product => {
+      if (product.id === cardItemDiv.getAttribute('data-id')
+        && product.color === cardItemDiv.getAttribute('data-color')) {
+        const newQuantity = parseInt(item.value)
+        const newPrice = (product.price / product.quantity) * newQuantity
+        cardItemDiv.querySelector('.cart__item__content__description').lastElementChild.innerHTML = `${newPrice} €`
+        return {
+          ...product,
+          quantity: newQuantity,
+          price: newPrice
+        }
+
+      }
+      return product;
+    })
+    localStorage.setItem("panier", JSON.stringify(panier))
+    updateCartValues()
+  })
 })
 
-
-
-
-
-// async function UpdateCart() {
-//     //OneProduct est un objet récupéré avec les informations du produit notamment son id, sa couleur(coloration), et sa quantité(number) (instance de classe)
-//     const OneProduct = await CreateProductForCart();
-//     const AllProducts = localStorage.getItem('Allproducts');
-//     const Cart =  AllProducts ? JSON.parse(AllProducts) : [];
-
-//     if (Cart.length == 0) {
-
-//         Cart.push(OneProduct);
-//         return Cart;
-
-//     } else {
-//        for (let CartParts of Cart) {   
-//             if (CartParts.id === OneProduct.id && CartParts.coloration === OneProduct.coloration) {
-//                 CartParts.number += OneProduct.number;
-//                 return Cart;
-//             } else {
-//                 Cart.push(OneProduct);
-//                 return Cart;
-//             }
-//         }
-//     }
-// }
-
-// async function UpdateStorage() {
-
-//     const Cart = await UpdateCart();
-
-//     localStorage.removeItem('Allproducts');
-//     localStorage.setItem('Allproducts', JSON.stringify(Cart));
-//     console.log("Le Storage s'est mis à jour.");
-
-
-
-
-
-
-
-
-
+/**
+ *
+ * Expects request to contain:
+ * contact: {
+ *   firstName: string,
+ *   lastName: string,
+ *   address: string,
+ *   city: string,
+ *   email: string
+ * }
+ * products: [string] <-- array of product _id
+ *
+ */
+formCart.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target)
+  const objectData = {
+    contact: Object.fromEntries(formData),
+    products: panier.map(product => product.id)
+  }
+  fetch(apiUrl, {
+    method: "POST",
+    headers: new Headers({ 'content-type': 'application/json' }),
+    body: JSON.stringify(objectData)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    }).catch((error) => {
+      console.log(error)
+    });
+})
 
 
 
@@ -107,5 +112,21 @@ function addPanierThird(panier) {
     </div>
   </article>
 `}
+
+function updateCartValues() {
+  let totalQuantity = 0
+  const elemTotalQuantity = document.getElementById("totalQuantity")
+  let totalPrice = 0
+  const elemTotalPrice = document.getElementById('totalPrice')
+  for (let item of panier) {
+    totalQuantity += item.quantity
+    totalPrice += item.price
+  }
+  elemTotalQuantity.innerHTML = totalQuantity
+  elemTotalPrice.innerHTML = totalPrice
+}
+
+
+
 
 
